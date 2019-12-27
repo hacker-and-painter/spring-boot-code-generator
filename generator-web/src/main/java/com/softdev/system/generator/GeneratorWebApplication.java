de7456e3f293b4b303e08c8041a3991e0ba3f63b
gaohanghang <1341947277@qq.com>
2019-12-27 15:06:17 +0800

    update

--- generator-web/src/main/java/com/softdev/system/generator/GeneratorWebApplication.java
+++ generator-web/src/main/java/com/softdev/system/generator/GeneratorWebApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableSwagger2Doc
 @SpringBootApplication
-@Slf4j
 public class GeneratorWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeneratorWebApplication.class,args);
-		log.info("项目启动启动成功！项目地址: http://localhost:1234");
 	}
 }
